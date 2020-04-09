3a4e7672f03c4bcb391974fefe0ecb48b7273d8b
Szekrényes Bence <scryzzoffical@gmail.com>
2020-04-09 21:41:59 +0200

    hopefully the contact message is working

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
 console.log("mükszik")
 
+$(document).ready(function () {
+  $("#contact-button").click(function (event) {
+    console.log("clicked")
 
+    var name = $('#name').val()
+    var email = $('#e-mail').val()
+    var comment = $('#comment').val()
+    var statusElement = $('.status')
+    statusElement.empty()
 
-
-$('#contact-button').click(() => {
-  $('#contact-button').toggleClass('hidden');
+    if(email.length > 5 && email.includes('@') && email.includes('.')) {
+      statusElement.append('Email is valid')
+    } else {
+      event.preventDefault()
+      statusElement.append('Email is not valid')
+    }
+  })
 })
