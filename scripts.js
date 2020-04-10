c3610073f78b2d368562352499d90fa828aeb410
Szekrényes Bence <scryzzoffical@gmail.com>
2020-04-10 17:27:35 +0200

    update

--- scripts.js
+++ scripts.js
@@ -16,4 +16,24 @@ $(document).ready(function () {
       event.preventDefault()
     }
   })
+  let imgtoggle
+  imgtoggle = 0
+  $('#r6s').fadeToggle(0)
+  $('.img-left').click(() => {
+    if (imgtoggle === 1 || imgtoggle === 0) {
+      $('#rocket-league').fadeToggle(300)
+      $('#r6s').fadeToggle(300)
+      imgtoggle = 0
+    }
+  })
+
+  $('.img-right').click(() => {
+    if (imgtoggle === 0 || imgtoggle === 1) {
+      $('#rocket-league').fadeToggle(300)
+      $('#r6s').fadeToggle(300)
+      imgtoggle = 1
+    }
+  })
+
+
 })
